9cb9f82d3a138037e76ee6597fb1df943d83e920
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-01-07 20:32:18 +0000

    closes exercise 2 and 2.1

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,27 +4,29 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+const ToggleOn = ({on, children}) => (on ? children : null)
+const ToggleOff = ({on, children}) => (on ? null : children)
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
+
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
+
+function Toggle(props) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(props.children, child => {
+    if (allowedTypes.includes(child.type) || typeof child.type === 'string') {
+      return React.cloneElement(child, {on, toggle})
+    }
+  })
 }
-
-const ToggleOn = (on, children) => (on ? children : null)
-const ToggleOff = (on, children) => (on ? null : children)
-const ToggleButton = (on, toggle) => <Switch on={on} onClick={toggle} />
 
 function App() {
   return (
     <div>
       <Toggle>
+        <span>Button status:</span>
+        <br/>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
         <ToggleButton />
