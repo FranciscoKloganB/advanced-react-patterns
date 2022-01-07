3a3b5b7718a43b28446108ced4deec75131ed5a4
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-01-07 22:34:30 +0000

    closes exercise 3.0

---
+++ src/exercise/context/toggle-context.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+const ToggleContext = React.createContext()
+ToggleContext.displayName = 'ToggleContext'
+
+function ToggleProvider({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+  
+  return <ToggleContext.Provider value={{on, toggle}}>{children}</ToggleContext.Provider>
+}
+
+function useToggle() {
+  const context = React.useContext(ToggleContext)
+  if (context === undefined) {
+    throw new Error(`useToggle must be used within a ToggleContext`)
+  }
+  return context
+}
+
+export {ToggleProvider as Toggle, useToggle}
