94db261f48ae2503343ce775c794fa808a7d08bf
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-01-08 08:56:16 +0000

    revert: add toggle outside of togglerProps for consumers who do not use the togglerProps api

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -8,7 +8,7 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  return {on, togglerProps: {'aria-pressed': on, onClick: toggle}}
+  return {on, toggle, togglerProps: {'aria-pressed': on, onClick: toggle}}
 }
 
 function App() {
