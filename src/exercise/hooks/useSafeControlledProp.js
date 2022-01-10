9464862d24bc00bf9cc83fcf7f0ca7c49847f6c4
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-01-10 16:39:59 +0000

    chore: perfect useSafeControlledProp

--- src/exercise/hooks/useSafeControlledProp.js
+++ src/exercise/hooks/useSafeControlledProp.js
@@ -1,23 +1,32 @@
 import * as React from 'react'
 
-export default function useSafeControlledProp({control, onChange, readOnly}) {
+export default function useSafeControlledProp({
+  control,
+  onChange,
+  readOnly,
+  context = {componentName: '', propName: ''},
+}) {
   const controlled = control != null
   const hasOnChange = !!onChange
 
   React.useEffect(() => {
     if (controlled && !hasOnChange && !readOnly) {
       console.warn(
-        'An `on` change was provided to `useToggle` without an `onChange` handler. This will render a read-only toggle. If you want it to be mutable, use `initialOn`. Otherwise, set either `onChange` or `readOnly`',
+        `A previously controlled component exists without an 'onChange' handler.
+         This will render a read-only toggle. Set either 'onChange' or 'readOnly'`,
+        context,
       )
     }
-  }, [controlled, hasOnChange, readOnly])
+  }, [context, controlled, hasOnChange, readOnly])
 
   const {current: wasControlled} = React.useRef(controlled)
   React.useEffect(() => {
     if (wasControlled !== controlled) {
       console.warn(
-        'Component using `useToggle` switch between Controlled and Uncontrolled state or vice-versa during its Lifetime. This should not happen',
+        `Component switched between Controlled and Uncontrolled state or vice-versa
+         during its Lifetime. This should not happen`,
+        context,
       )
     }
-  }, [controlled, wasControlled])
+  }, [context, controlled, wasControlled])
 }
